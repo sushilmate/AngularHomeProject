4d292301e496ff75331d06180851f0f09bbd0b4d
Sushil Mate <sushilmate007@gmail.com>
2018-08-08 06:55:59 +0530

    Adding import/export excel feature to UI

--- CVAHelper/ClientApp/src/app/home/home.component.ts
+++ CVAHelper/ClientApp/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, ViewChild } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { AgGridNg2 } from "ag-grid-angular";
 import { GidGsrMappingViewModel } from '../../common/types';
+import * as XLSX from 'xlsx';
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -13,6 +14,7 @@ const httpOptions = {
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
   public rowData: GidGsrMappingViewModel[];
   @ViewChild("agGrid") agGrid: AgGridNg2;
 
@@ -31,14 +33,43 @@ export class HomeComponent {
   defaultColDef = {
     editable: true
   }
-  getSelectedRows(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+
+  arrayBuffer: any;
+  file: File;
+
+  incomingfile(event) {
+    this.file = event.target.files[0];
+  }
+
+  Upload() {
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      this.arrayBuffer = fileReader.result;
+      var data = new Uint8Array(this.arrayBuffer);
+      var arr = new Array();
+      for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+      var bstr = arr.join("");
+      var workbook = XLSX.read(bstr, { type: "binary" });
+      var first_sheet_name = workbook.SheetNames[0];
+      var worksheet = workbook.Sheets[first_sheet_name];
+      //this.rowData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+      alert(JSON.stringify(XLSX.utils.sheet_to_json(worksheet, { header: 1 })));
+    }
+    fileReader.readAsArrayBuffer(this.file);
+  }
+
+
+  getSelectedRows() {
     const selectedNodes = this.agGrid.api.getSelectedNodes();
     const selectedData = selectedNodes.map(node => node.data);
-    //const selectedDataStringPresentation = selectedData.map(node => node.gsr + ' ' + node.gid).join(', ');
-    //alert(selectedDataStringPresentation);
 
-    http.post(baseUrl + 'api/GidGsrMapping/UpdateGidGsrMappings', selectedData, httpOptions).subscribe(result => {
-      console.log(result);
-    }, error => console.log('There was an error: '));
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(selectedData);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, 'SheetJS.xlsx');
   }
 }
