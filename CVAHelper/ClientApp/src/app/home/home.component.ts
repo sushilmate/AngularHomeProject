19aa0afc431dbd8ad680e8be18487e8f8fcfcae2
Sushil Mate <sushilmate007@gmail.com>
2018-08-15 14:22:47 +0530

    upgrading the packages in the project.

--- CVAHelper/ClientApp/src/app/home/home.component.ts
+++ CVAHelper/ClientApp/src/app/home/home.component.ts
@@ -31,7 +31,7 @@ export class HomeComponent {
 
   ngOnInit() {
     this.spinnerService.show();
-    this.http.get<GidGsrMappingModel[]>('api/GidGsrMapping/GetGidGsrMapping1').subscribe(result => {
+    this.http.get<GidGsrMappingModel[]>('api/GidGsrMapping/GetGidGsrMapping').subscribe(result => {
       this.rowData = result;
       this.spinnerService.hide();
     });
